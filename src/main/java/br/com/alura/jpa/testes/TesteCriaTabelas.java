e70ac3c190dee65b41c0582d840005fb2a677bc2
Adriano Avelino <adriano-st777@hotmail.com>
2021-01-25 21:25:40 -0300

    adiciona exemplo de criação de tabela

---
+++ src/main/java/br/com/alura/jpa/testes/TesteCriaTabelas.java
@@ -0,0 +1,14 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TesteCriaTabelas {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		emf.createEntityManager();
+		emf.close();
+	}
+
+}
